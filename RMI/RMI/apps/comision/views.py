345b6a719dd021254adc51d07b517fc210ec8d62
Diego <kaoxdc@gmail.com>
2017-06-10 03:19:11 -0500

    view file

--- RMI/RMI/apps/comision/views.py
+++ RMI/RMI/apps/comision/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .forms import *
 from django.core.mail import send_mail, EmailMessage
-
+from django.conf import settings
 # Create your views here.
 def comision_view (request):
 
@@ -14,9 +14,26 @@ def comision_view (request):
 				i = form_Com.save(commit = False)
 				i.save()
 				info = "Informacion Guardada con Exito!" 
-
 			except:
-				info = "Debe completar todos los campos" 
+				info = "Debe completar todos los campos"
+			
+			texto = i.nombres_comisionado+" "+ i.identificacion+" "+i.email 
+			mail = EmailMessage('Registro de comision '+i.nombres_comisionado, texto, 'serviciosdocumentosctpi@gmail.com',['dfprado4@misena.edu.co'] )
+			#mail.attach(firma.name, firma.read(), firma.content_type)
+			file_path = settings.MEDIA_ROOT + str(i.firma)
+			print(file_path)
+			fd = open("%s"%(file_path),"rb")
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+			print (firma.name, firma.read(), firma.content_type)
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+			#mail.attach_file(i.firma)
+			mail.attach(firma.name, fd.read(), firma.content_type)
+			x= mail.send()
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+			print x
+			print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+			fd.close()
+			form_Com = Comision_form()
 		else:		
 			info = "Debe completar todos los campos" 
 	if request.method == "GET":
