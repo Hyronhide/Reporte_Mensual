a0718160067ca71191d2e47198304de29f0ca3c9
diaval90 <kfrivas@misena.edu.co>
2017-08-09 14:27:38 -0500

    config

--- RMI/RMI/settings.py
+++ RMI/RMI/settings.py
@@ -179,6 +179,6 @@ EMAIL_USE_TLS   = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'serviciosdocumentosctpi@gmail.com'
-EMAIL_HOST_PASSWORD = 'senactpi2016'
+EMAIL_HOST_PASSWORD = 'kenneth1234'
 DEFAUT_FROM_EMAIL = 'serviciosdocumentosctpi@gmail.com'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
